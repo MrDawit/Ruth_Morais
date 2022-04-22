0bea764b921341b878b73f127d7e71db784e68e9
Shalom Dawit <Shalom.Dawit@gmail.com>
2022-04-22 00:41:39 -0400

    updated aboutPage and portfolioPage

--- client/src/pages/aboutPage.jsx
+++ client/src/pages/aboutPage.jsx
@@ -1,12 +1,6 @@
 import React, { useContext } from 'react';
 import { motion } from 'framer-motion';
-// import Spacer from '../components/Spacer';
-import Tilt from '../components/Tilt/Tilt.js';
 import "../assets/css/aboutPage.css";
-import SkillIconList from "../components/SkillIconList/index"
-import { SkillIconProvider } from "../utils/IconContext";
-// import { SkillIconContext } from "../utils/IconContext";
-import resume from '../assets/files/RMorais.docx';
 
 
 const AboutPage = () => {
@@ -32,29 +26,14 @@ const AboutPage = () => {
                 <main className="col-xs-12 col-lg-10">
                     <div id="skillsAbout">
 
-                        <h3>Roles </h3>
-                        <article >Marketing Project Manager | Sales Manager | Product Innovation</article>
-                        <SkillIconProvider>
-                            <SkillIconList />
-                        </SkillIconProvider>
-
-                        <div id="bio">
-
-                            <h1>Bio</h1>
-                            <img alt={"Shalom Dawit"} src={require("../assets/img/aria_light.jpg").default} className="image-wrapper float-left img-responsive pr-2" id="bioImage" />
-
-                            <article className="pull-right">
-                                <span> </span> I am a strategic, collaborative and data-driven Marketing Project Manager and Sales Manager leveraging nearly 10 years of experience in supporting strategic visions through driving process implementation, delivering comprehensive training for staff and clients, and leading organizational process improvements. As a dynamic team leader, I have been recognized for driving sales, facilitating event coordination, analyzing data, and streamlining processes and procedures.
-                            </article>
-                            {/* <a id="purple" href={"../assets/img/pic0.jpg"} target="_blank" rel="noreferrer">RESUME</a> */}
-                            <a id="purple" href={resume} download="Ruth Morais Resume"> Resume </a>
-                        </div>
                     </div>
-                </main>
-                {/* <Spacer /> */}
-                <Tilt />
 
-                {/*  <Footer /> */}
+                </main>
+
+         <modal>
+
+         </modal>
+
             </div>
         </motion.div>
     );
--- client/src/pages/portfolioPage.jsx
+++ client/src/pages/portfolioPage.jsx
@@ -24,14 +24,16 @@ const ProjectListPage = () => {
         }}> 
         <div className="bod">
         <main className="col-xs-12 col-lg-10">
-        <h1>Portfolio</h1>
+    <h3 >- Ruth is a Marketing Project Manager, Sales Manager and Product Innovationist in the USA</h3> 
       
                 <ProjectProvider>
                   
                     <ProjectList/>
                 
                 </ProjectProvider>
-              
+                <article className="pull-center">
+                                 I am a strategic, collaborative and data-driven Marketing Project Manager and Sales Manager leveraging nearly 10 years of experience in supporting strategic visions through driving process implementation, delivering comprehensive training for staff and clients, and leading organizational process improvements. As a dynamic team leader, I have been recognized for driving sales, facilitating event coordination, analyzing data, and streamlining processes and procedures.
+                            </article>
             </main>
        
             </div>
