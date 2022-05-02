83c0eae139edbc6b4297029e31394d4d088a86fc
Shalom Dawit <Shalom.Dawit@gmail.com>
2022-05-02 19:14:54 -0400

    clean code including deleting useless comments

--- client/src/components/ProjectCard/index.js
+++ client/src/components/ProjectCard/index.js
@@ -1,16 +1,12 @@
 import React from "react";
 import "./style.css"
 import LazyLoad from 'react-lazy-load';
-import Spacer2 from "../Spacer2";
 
-const ProjectCard = ({ name, descriptionShort, imageURL, deployURL, gitHubRepo, loginInfo, skills }) => {
+const ProjectCard = ({ name, imageURL, deployURL, gitHubRepo }) => {
 
   return (
     <>
-    {/* <Spacer2> */}
-      {/* <div className="card z-depth-3"> */}
       <div className="card">
-        {/* <a target="_blank" rel="noreferrer" href={deployURL}> */}
           <div className="card-image">
           <a target="_blank" rel="noreferrer" href={deployURL}>
           <LazyLoad offsetVertical={1000} onContentVisible={false}>
@@ -20,17 +16,9 @@ const ProjectCard = ({ name, descriptionShort, imageURL, deployURL, gitHubRepo, 
               <div className="text">{name}</div>
             </div></a>
           </div>
-        {/* </a> */}
-        {/* <div className="overlay"><div className="text">{name}</div></div> */}
         <a target="_blank" rel="noreferrer" href={gitHubRepo}>
           <span className="card-title"> {name}</span>
           </a>
-
-        {/* <div className="card-info">
-          <p id="projDescr">({descriptionShort})</p>
-          <span>Tools Used: </span><p id="skills">{skills}</p>
-          <p id="login_info">{loginInfo}</p>
-        </div> */}
 
         <div className="card-action">
 
@@ -39,7 +27,6 @@ const ProjectCard = ({ name, descriptionShort, imageURL, deployURL, gitHubRepo, 
 
       </div>
       <br /><br />
-      {/* </Spacer2> */}
     </>
   )
 
