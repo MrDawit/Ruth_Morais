68f8853e0fdceb4308d361155bb1165b35167432
Shalom Dawit <Shalom.Dawit@gmail.com>
2022-05-02 20:15:32 -0400

    more style editing on ProjectCard for portfolioPage

--- client/src/components/ProjectList/index.js
+++ client/src/components/ProjectList/index.js
@@ -1,6 +1,7 @@
 import React, {  useContext } from "react";
 import ProjectCard from "../ProjectCard";
 import { ProjectContext } from "../../utils/ProjectContext";
+import "./style.css";
 
 
 const ProjectList = () => {
@@ -10,7 +11,7 @@ const ProjectList = () => {
             <div className="row">
                 {projects.map(project => (
                    
-                     <div className="col-sm-12 col-md-6 " key={project.id}>
+                     <div className="col-sm-12 col-md-6 " key={project.id} id="projectList">
                         
                         {/* <ProjectCard name={project.name} imageURL={project.imageURL} descriptionShort={project.descriptionShort} gitHubRepo={project.gitHubRepo} deployURL={project.deployURL} loginInfo ={project.loginInfo} skills ={project.skills}/> */}
                         <ProjectCard name={project.name} imageURL= {project.imageURL} deployURL={project.deployURL}/>
