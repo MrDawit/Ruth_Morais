9f7d4bf02a7ef0353d3ed110df386e4dbe878a3e
Shalom Dawit <Shalom.Dawit@gmail.com>
2022-05-11 00:24:02 -0400

    used flexbox for element containter on aboutPage

--- client/src/pages/aboutPage.jsx
+++ client/src/pages/aboutPage.jsx
@@ -1,8 +1,7 @@
-import React from // ,{ useContext }
-"react";
+import React from "react"; // ,{ useContext }
 import { motion } from "framer-motion";
 import "../assets/css/aboutPage.css";
-// import Footer from '../components/Footer/index';
+import Footer from "../components/Footer/index";
 
 const AboutPage = () => {
   return (
@@ -50,54 +49,59 @@ const AboutPage = () => {
         <br />
         <br />
         <br />
-
-        <div id="bioSection">
-          <h2 id="aboutH3">Bio</h2>
-          <div id="bioAbout">
-            I am a strategic, collaborative and data-driven Marketing Project
-            Manager and Sales Manager leveraging nearly 10 years of experience
-            in supporting strategic visions through driving process
-            implementation, delivering comprehensive training for staff and
-            clients, and leading organizational process improvements. As a
-            dynamic team leader, I have been recognized for driving sales,
-            facilitating event coordination, analyzing data, and streamlining
-            processes and procedures
+        <div id="bioAndImageAboutSection">
+          <div id="bioSection">
+            <h2 id="aboutH3">Bio</h2>
+            <div id="bioAbout">
+              I am a strategic, collaborative and data-driven Marketing Project
+              Manager and Sales Manager leveraging nearly 10 years of experience
+              in supporting strategic visions through driving process
+              implementation, delivering comprehensive training for staff and
+              clients, and leading organizational process improvements. As a
+              dynamic team leader, I have been recognized for driving sales,
+              facilitating event coordination, analyzing data, and streamlining
+              processes and procedures
+            </div>
           </div>
+
+          <img
+            id="imageAbout"
+            alt="about Ruth"
+            src={require("../assets/img/aria_light.jpg").default}
+          />
         </div>
-
-        <img
-          id="imageAbout"
-          alt="about Ruth"
-          src={require("../assets/img/aria_light.jpg").default}
-        />
-
-        <div id="skillsSection">
-          <h5 id="aboutH6">Noteworthy Skills</h5>
-          <div id="skillsAbout">
-            Management, Project Management, Salesforce.com, Account Management,
-            Sales Process, Sales, Solution Selling, Analytics, Enterprise
-            Software, Sales Operations, Strategy, Lead Generation, Business
-            Analysis, Customer Relationship Management (CRM), Microsoft Office,
-            Training, Team Building, Customer Service, Business Process
-            Improvement, Marketing, Analytical Skills, Training & Development,
-            Microsoft Excel, Financial Analysis, FinTech, Product Management,
-            Portfolio Management, Product Road Mapping, Product integration, API
-            Development, Customer Retention, Key Performance Indicators,
-            Employee Learning & Development, Business Development, Process
-            Improvement, Relationship Building, Data Analysis, Marketing
-            Strategy, Vendor Management, Strategic Planning, Sales
-            Presentations, Revenue & Profit Growth, User Experience (UX),
-            Product Launch, Project Portfolio Management, Budget Management,
-            Emotional Intelligence, Non-Fungible Tokens (NFTs), Product Owner
+        <br/>
+        <br/>
+        <div id="skillsAndQuoteAboutSection">
+          <div id="skillsSection">
+            <h5 id="aboutH6">Noteworthy Skills</h5>
+            <div id="skillsAbout">
+              Management, Project Management, Salesforce.com, Account
+              Management, Sales Process, Sales, Solution Selling, Analytics,
+              Enterprise Software, Sales Operations, Strategy, Lead Generation,
+              Business Analysis, Customer Relationship Management (CRM),
+              Microsoft Office, Training, Team Building, Customer Service,
+              Business Process Improvement, Marketing, Analytical Skills,
+              Training & Development, Microsoft Excel, Financial Analysis,
+              FinTech, Product Management, Portfolio Management, Product Road
+              Mapping, Product integration, API Development, Customer Retention,
+              Key Performance Indicators, Employee Learning & Development,
+              Business Development, Process Improvement, Relationship Building,
+              Data Analysis, Marketing Strategy, Vendor Management, Strategic
+              Planning, Sales Presentations, Revenue & Profit Growth, User
+              Experience (UX), Product Launch, Project Portfolio Management,
+              Budget Management, Emotional Intelligence, Non-Fungible Tokens
+              (NFTs), Product Owner
+            </div>
           </div>
-        </div>
 
-        <div id="quoteAbout">
-          “A very high standard that helps push teams forward to produce the
-          best possible outcomes. As product manager, she owns the process and
-          understands how to communicate in all directions so that everyone in
-          the organization stays on the same page.” — Bradley, Vice President @
-          Webster
+          <div id="quoteAbout">
+            “A very high standard that helps push teams forward to produce the
+            best possible outcomes. As product manager, she owns the process and
+            understands how to communicate in all directions so that everyone in
+            the organization stays on the same page.” — Bradley, Vice President
+            @ Webster
+          </div>
         </div>
       </main>
 
