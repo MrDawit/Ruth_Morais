586f3253fea2f656e6fb9b3cb485dd6b64599204
Shalom Dawit <Shalom.Dawit@gmail.com>
2022-06-24 23:37:56 -0400

    added to PhotoCollectionContext

--- client/src/utils/PhotoCollectionContext.js
+++ client/src/utils/PhotoCollectionContext.js
@@ -1,5 +1,7 @@
 import React, { useState, createContext } from "react";
-
+const tomWeddingPhoto = {
+    url: "https://igdownloader.com/file?id=aHR0cHM6Ly9zY29udGVudC1hdGwzLTEuY2RuaW5zdGFncmFtLmNvbS92L3Q1MS4yODg1LTE1LzYyNDQ4ODM5XzI2NzYxNDY4NzQyMzUxNV8yNzQ0NzMyNDM5MDU2MjIxNTc2X24uanBnP3N0cD1kc3QtanBnX2UzNSZfbmNfaHQ9c2NvbnRlbnQtYXRsMy0xLmNkbmluc3RhZ3JhbS5jb20mX25jX2NhdD0xMDcmX25jX29oYz03OGZUeEtncXNJb0FYLU5TWXVEJmVkbT1BQUJCdmpVQkFBQUEmY2NiPTctNSZvaD0wMF9BVDlFUW5Eb0JHZTlEaTJRZ1YzTTlxYjRYTW12REZ1Y2ZhWl9xTGx3bm5sVEVnJm9lPTYyQkI5MUU5Jl9uY19zaWQ9ODNkNjAz"
+}
 
 export const PhotoCollectionContext = createContext()
 
@@ -14,11 +16,36 @@ export const PhotoCollectionProvider = props => {
             id: 1
         },
         {
-            name: "aria_light",
-            localAddress: require("../assets/img/aria_light.jpg").default,
+            name: "tomWeddingPhoto",
+            localAddress: tomWeddingPhoto.url,
             message:"@Tom's Wedding",
             id: 2
         },
+        {
+            name: "aria_light",
+            localAddress: require("../assets/img/aria_light.jpg").default,
+            message:"With the scarf!",
+            id: 3
+        },
+        {
+            name: "Screenshot 2022-06-23 at 00-08-04 Y",
+            localAddress: require("../assets/img/Screenshot 2022-06-23 at 00-08-04 Y.png").default,
+            message:"Palm Trees!",
+            id: 4
+        },
+        {
+            name: "Screenshot 2022-06-23 at 00-09-50 Y",
+            localAddress: require("../assets/img/Screenshot 2022-06-23 at 00-09-50 Y.png").default,
+            message:"Fourth of July Food Truckin'",
+            id: 5
+        },
+        {
+            name: "Screenshot 2022-06-23 at 00-09-24 Y",
+            localAddress: require("../assets/img/Screenshot 2022-06-23 at 00-09-24 Y.png").default,
+            message:"Ramen, Ramen, Ramen",
+            id: 6
+        },
+
 
     ]);
     return (
