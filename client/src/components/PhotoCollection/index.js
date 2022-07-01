2b8258969abeaf86e6a1a41850c3a58957fd3a0b
Shalom Dawit <Shalom.Dawit@gmail.com>
2022-06-30 23:31:31 -0400

    close modal working:moved modal code from PhotoCollectionList to PhotoCollection

--- client/src/components/PhotoCollection/index.js
+++ client/src/components/PhotoCollection/index.js
@@ -1,10 +1,57 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import "./style.css"
+import { Modal, Button } from "react-bootstrap";
+import modalImage from "../../assets/img/AfricaIsWoke.jpg";
 
 
 const PhotoCollection = ({ name, photoUrl }) => {
+    const [modal, setModal] = useState(false);
+    const [modalFullscreen, setModalFullscreen] = useState(true);
+    // const renderModal = () => {
+    //     setModal(false);
+    //     //     setModalFullscreen(false);
+    //     console.log("this too should be working");
+    //   };
     return (
-        <img alt={name} src={photoUrl} className="PhotoCollection" />
+        <>
+
+        <img alt={name} src={photoUrl} onClick={()=>{setModal(true)} } className="PhotoCollection" />
+ {modal && (
+          <Modal
+            // className="modal"
+            //     show={modal}
+            show={true}
+            fullscreen={modalFullscreen}
+            //     onHide={renderModal()}
+          >
+            <Modal.Header
+            //closeButton is not working
+            //       closeButton
+            >
+              <Modal.Title>Modal</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              {" "}
+              <img
+                src={modalImage}
+                // src={modalImage.src}
+                width="200"
+                className="img-fluid"
+                alt="ImgTest"
+              />
+            </Modal.Body>
+            <Modal.Footer>
+              <Button
+                onClick={() => {
+                  setModal(false);
+                }}
+              >
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        )}
+        </>
     )
 };
 
--- client/src/components/PhotoCollectionList/index.js
+++ client/src/components/PhotoCollectionList/index.js
@@ -1,80 +1,16 @@
-import React, { useState, useContext } from "react";
-import { Modal, Button } from "react-bootstrap";
+import React, { useContext } from "react";
+// import { Modal, Button } from "react-bootstrap";
 import PhotoCollection from "../PhotoCollection";
 import { PhotoCollectionContext } from "../../utils/PhotoCollectionContext";
-import modalImage from "../../assets/img/AfricaIsWoke.jpg";
 
 const PhotoCollectionList = () => {
   const [imgPhotos] = useContext(PhotoCollectionContext);
-  const [modal, setModal] = useState(false);
-  const [modalFullscreen, setModalFullscreen] = useState(true);
-
-  const renderModal = () => {
-    setModal(false);
-    setModalFullscreen(false);
-    console.log("this too should be working");
-  };
 
   return imgPhotos.map((imgPhoto) => {
     console.log(`imgPhoto is = ${imgPhoto.id}`);
     return (
       <div key={imgPhoto.id}>
-        <Button
-          onClick={() => {
-            setModal(true);
-          }}
-        >
-          <PhotoCollection
-            name={imgPhoto.name}
-            photoUrl={imgPhoto.localAddress}
-        //     key={imgPhoto.id}
-          />
-
-          <Modal
-            // className="modal"
-            show={modal}
-            fullscreen={modalFullscreen}
-            onHide={renderModal}
-          >
-            <Modal.Header
-            //closeButton is not working
-            //       closeButton
-            >
-              <Modal.Title>Modal</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-              {" "}
-              <img
-                src={modalImage}
-                // src={modalImage.src}
-                width="200"
-                className="img-fluid"
-                alt="ImgTest"
-              />
-            </Modal.Body>
-            <Modal.Footer>
-              <Button
-                onClick={() => {
-                  renderModal();
-                  console.log("should be working");
-                  setModal(false);
-                }}
-                // onRequestClose={() => setModal(false)}
-              >
-                Close
-              </Button>
-            </Modal.Footer>
-          </Modal>
-        </Button>
-
-        {/* <Button
-          style={{ backgroundColor: "red" }}
-          onClick={() => {
-            setModal(true);
-          }}
-        >
-          Click Me
-        </Button> */}
+        <PhotoCollection name={imgPhoto.name} photoUrl={imgPhoto.localAddress}/>
       </div>
     );
   });
