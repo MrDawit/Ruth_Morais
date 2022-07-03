222915480932751902040768a6f8e64cd5b300d2
Shalom Dawit <Shalom.Dawit@gmail.com>
2022-07-03 11:12:35 -0400

    removed PhotoCollection from code loop, Carousel open&close on photo id

--- client/src/components/PhotoCollection/index.js
+++ client/src/components/PhotoCollection/index.js
@@ -1,144 +1,200 @@
 import React, { useState, useContext } from "react";
 import "./style.css";
-import { Modal, Button, Carousel, Image, } from "react-bootstrap";
-import modalImage from "../../assets/img/AfricaIsWoke.jpg";
+import { Modal, Button, Carousel, Image } from "react-bootstrap";
 import { PhotoCollectionContext } from "../../utils/PhotoCollectionContext";
 import PhotoCollectionList from "../PhotoCollectionList";
+import  indexx from "../PhotoCollectionList";
 
-const PhotoCollection = ({ name, photoUrl }) => {
-    const [imgPhotos] = useContext(PhotoCollectionContext);
+
+// Plan of Attack (A or B) :
+// A) fix modal close that should be working, then move onto B.
+// B) create carousel and function that starts at clicked image and continues and wraps around index of images, then fix modal close issue (if one still exists)
+
+const PhotoCollection = ({ name, photoUrl, photoKey, message }) => {
+  const [imgPhotos] = useContext(PhotoCollectionContext);
 
   const [modal, setModal] = useState(false);
   const [modalFullscreen, setModalFullscreen] = useState(true);
-  const [imageCarousel, setImageCarousel] = useState(false);
-    const [index, setIndex] = useState(imgPhotos.id);
+  const [imageCarousel, setImageCarousel] = useState(true);
+ 
+//   const [pCKey, setPCKey] = useState(0);
+// const makeKeylocal = () => {
+//     setPCKey(key);
+//     console.log(`key inside makeKeylocal = ${key}`);
+// }
+//makeKeylocal();
 
-  const handleSelect = (selectedIndex, e) => {
-    setIndex(selectedIndex);
-  };
+//   const handleSelect = (selectedIndex, e) => {
+//     setIndex2(selectedIndex);
+//   };
   // const renderModal = () => {
   //     setModal(false);
   //     //     setModalFullscreen(false);
   //     console.log("this too should be working");
   //   };
+//   const [index2, setIndex2] = useState(pCKey);
+
+
+// const [index2, setIndex2] = useState(photoKey);
   return (
     <>
       <img
         alt={name}
         src={photoUrl}
         onClick={() => {
-        //   setModal(true);
-        setImageCarousel(true);
+          //   setModal(true);
+          setImageCarousel(true);
+          console.log(`imageCarousel from PhotoCollection is ${imageCarousel}`);
         }}
         className="PhotoCollection"
       />
 
-{imageCarousel &&
-<Modal     show={true}
-          fullscreen={modalFullscreen} >
-
- <Carousel activeIndex={index} onSelect={handleSelect}>
-           {/* image 1  */}
-          <Carousel.Item>
-            <img
-              className="imgCarousel"
-              // className="d-block w-100 "
-              src={require("../../assets/img/aria.jpg")}
-              alt="First slide"
-            />
-            <Carousel.Caption>
-              <h3 id="carouselH3">Low Def</h3>
-              <p> </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-          {/* image 2  */}
-          <Carousel.Item>
-            <Image
-              className="imgCarousel"
-              // className="d-block w-100"
-              src="https://igdownloader.com/file?id=aHR0cHM6Ly9zY29udGVudC1hdGwzLTEuY2RuaW5zdGFncmFtLmNvbS92L3Q1MS4yODg1LTE1LzYyNDQ4ODM5XzI2NzYxNDY4NzQyMzUxNV8yNzQ0NzMyNDM5MDU2MjIxNTc2X24uanBnP3N0cD1kc3QtanBnX2UzNSZfbmNfaHQ9c2NvbnRlbnQtYXRsMy0xLmNkbmluc3RhZ3JhbS5jb20mX25jX2NhdD0xMDcmX25jX29oYz03OGZUeEtncXNJb0FYLU5TWXVEJmVkbT1BQUJCdmpVQkFBQUEmY2NiPTctNSZvaD0wMF9BVDlFUW5Eb0JHZTlEaTJRZ1YzTTlxYjRYTW12REZ1Y2ZhWl9xTGx3bm5sVEVnJm9lPTYyQkI5MUU5Jl9uY19zaWQ9ODNkNjAz"
-              alt="Second slide"
-            />
-            <Carousel.Caption>
-              <h3 id="carouselH3">@ Tom's Wedding</h3>
-              <p> </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-           {/* image 3 */}
-          <Carousel.Item>
-            <Image
-              // fluid='true'
-              // thumbnail='true'
-              className="imgCarousel"
-              // className="d-block w-100"
-              src={require("../../assets/img/aria_light.jpg")}
-              alt="Third slide"
-            />
-            <Carousel.Caption>
-              <h3 id="carouselH3">With the scarf!</h3>
-              <p></p>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-           {/* image 4  */}
-          <Carousel.Item>
-            <img
-              className="imgCarousel"
-              // className="d-block w-100 "
-              src={
-                require("../../assets/img/Screenshot 2022-06-23 at 00-08-04 Y.png")
-              }
-              alt="Fourth slide"
-            />
-            <Carousel.Caption>
-              <h3 id="carouselH3">Palm Trees! </h3>
-              <p> </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-          {/* image 5  */}
-          <Carousel.Item>
-            <Image
-              className="imgCarousel"
-              // className="d-block w-100"
-              src={
-                require("../../assets/img/Screenshot 2022-06-23 at 00-09-50 Y.png")
-              }
-              alt="Fifth slide"
-            />
-            <Carousel.Caption>
-              <h3 id="carouselH3">Fourth of July Food Truckin' </h3>
-              <p> </p>
-            </Carousel.Caption>
-          </Carousel.Item>
-
-          {/* image 6  */}
-          <Carousel.Item>
-            <Image
-              // fluid='true'
-              // thumbnail='true'
-              className="imgCarousel"
-              // className="d-block w-100"
-              src={
-                require("../../assets/img/Screenshot 2022-06-23 at 00-09-24 Y.png")
-              }
-              alt="Sixth slide"
-            />
-            <Carousel.Caption>
-              <h3 id="carouselH3">Ramen, Ramen, Ramen </h3>
-              <p></p>
-            </Carousel.Caption>
-          </Carousel.Item>
-            </Carousel> 
-            </Modal>
-
-}
+      {/* {imageCarousel && (
+        <Modal
+          show={true}
+          fullscreen={modalFullscreen}
+          style={{ backgroundColor: "black" }}
+        >
+              <Modal.Header
+          //closeButton is not working
+          //       closeButton
+          >
+            <Modal.Title>{photoKey}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body >
+          <Carousel
+            style={{ backgroundColor: "black" }}
+            activeIndex={index2}
+            onSelect={handleSelect}
+            // variant="dark"
+            variant='light'
+            prevLabel={"Previous"}
+            interval={null}
+          >
 
 
+     // image 1   
+    <Carousel.Item>
+              <img
+                className="imgCarousel"
+                // className="d-block w-100 "
+                src={photoUrl}
+                alt={name}
+              />
+              <Carousel.Caption>
+                <h3 id="carouselH3">{message}</h3>
+                <p> </p>
+              </Carousel.Caption>
+            </Carousel.Item> */}
 
-      {modal && (
+          
+
+
+            {/* // image 1  
+            <Carousel.Item>
+              <img
+                className="imgCarousel"
+                // className="d-block w-100 "
+                src={require("../../assets/img/aria.jpg")}
+                alt="First slide"
+              />
+              <Carousel.Caption>
+                <h3 id="carouselH3">Low Def</h3>
+                <p> </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+
+             // image 2   
+            <Carousel.Item>
+              <Image
+                className="imgCarousel"
+                // className="d-block w-100"
+                src={require("../../assets/img/tomWedding.jpg")}
+                alt="Second slide"
+              />
+              <Carousel.Caption>
+                <h3 id="carouselH3">@ Tom's Wedding</h3>
+                <p> </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+
+            // image 3 
+            <Carousel.Item>
+              <Image
+                // fluid='true'
+                // thumbnail='true'
+                className="imgCarousel"
+                // className="d-block w-100"
+                src={require("../../assets/img/aria_light.jpg")}
+                alt="Third slide"
+              />
+              <Carousel.Caption>
+                <h3 id="carouselH3">With the scarf!</h3>
+                <p></p>
+              </Carousel.Caption>
+            </Carousel.Item>
+
+             // image 4  
+            <Carousel.Item>
+              <img
+                className="imgCarousel"
+                // className="d-block w-100 "
+                src={require("../../assets/img/Screenshot 2022-06-23 at 00-08-04 Y.png")}
+                alt="Fourth slide"
+              />
+              <Carousel.Caption>
+                <h3 id="carouselH3">Palm Trees! </h3>
+                <p> </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+
+             // image 5  
+            <Carousel.Item>
+              <Image
+                className="imgCarousel"
+                // className="d-block w-100"
+                src={require("../../assets/img/Screenshot 2022-06-23 at 00-09-50 Y.png")}
+                alt="Fifth slide"
+              />
+              <Carousel.Caption>
+                <h3 id="carouselH3">Fourth of July Food Truckin' </h3>
+                <p> </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+
+           { // image 6   
+            <Carousel.Item>
+              <Image
+                // fluid='true'
+                // thumbnail='true'
+                className="imgCarousel"
+                // className="d-block w-100"
+                src={require("../../assets/img/Screenshot 2022-06-23 at 00-09-24 Y.png")}
+                alt="Sixth slide"
+              />
+              <Carousel.Caption>
+                <h3 id="carouselH3">Ramen, Ramen, Ramen </h3>
+                <p></p>
+              </Carousel.Caption>
+            </Carousel.Item> */}
+
+
+          {/* </Carousel>
+          </Modal.Body>
+          <Modal.Footer>
+          <Button
+              onClick={() => {
+                // setModal(false);
+                setImageCarousel(false);
+              }}
+            >
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      )} */}
+
+      {/* {modal && (
         <Modal
           // className="modal"
           //     show={modal}
@@ -171,7 +227,7 @@ const PhotoCollection = ({ name, photoUrl }) => {
             </Button>
           </Modal.Footer>
         </Modal>
-      )}
+      )} */}
     </>
   );
 };
--- client/src/components/PhotoCollectionList/index.js
+++ client/src/components/PhotoCollectionList/index.js
@@ -1,17 +1,99 @@
-import React, { useContext } from "react";
-// import { Modal, Button } from "react-bootstrap";
+import React, { useState, useContext } from "react";
 import PhotoCollection from "../PhotoCollection";
 import { PhotoCollectionContext } from "../../utils/PhotoCollectionContext";
+import { Modal, Button, Carousel, Image } from "react-bootstrap";
 
 const PhotoCollectionList = () => {
   const [imgPhotos] = useContext(PhotoCollectionContext);
-
+  const [index2, setIndex2] = useState(0);
+  const handleSelect = (selectedIndex, e) => {
+    setIndex2(selectedIndex);
+  };
+  const [imageCarousel, setImageCarousel] = useState(false);
+  const handleClick = () => {
+    setImageCarousel(true);
+    
+    console.log(`index onClick is ${index2}`);
+  };
   return imgPhotos.map((imgPhoto) => {
     console.log(`imgPhoto is = ${imgPhoto.id}`);
+    console.log(imageCarousel);
     return (
-      <div key={imgPhoto.id}>
-        <PhotoCollection name={imgPhoto.name} photoUrl={imgPhoto.localAddress}/>
-      </div>
+      <>
+        {/* <PhotoCollection
+          name={imgPhoto.name}
+          photoUrl={imgPhoto.localAddress}
+          key={imgPhoto.id}
+          message={imgPhoto.message}
+          photoKey={imgPhoto.id}
+          onClick={() => {
+            handleClick();
+            //     setImageCarousel(true);
+                setIndex2(imgPhoto.id);
+            //     console.log(`index onClick is ${index2}`);
+          }}
+        /> */}
+           <img
+           key={imgPhoto.id}
+        alt={imgPhoto.name}
+        src={imgPhoto.localAddress}
+        onClick={() => {
+          //   setModal(true);
+          setImageCarousel(true);
+          console.log(`imageCarousel from PhotoCollection is ${imageCarousel}`);
+        }}
+        className="PhotoCollection"
+      />
+        {imageCarousel && (
+          <Modal
+            show={true}
+            fullscreen={true}
+            style={{ backgroundColor: "black" }}
+          >
+            <Modal.Header
+            //closeButton is not working
+            //       closeButton
+            >
+              <Modal.Title>{imgPhoto.id}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Carousel
+                style={{ backgroundColor: "black" }}
+                activeIndex={index2}
+                onSelect={handleSelect}
+                // variant="dark"
+                variant="light"
+                prevLabel={"Previous"}
+                interval={null}
+              >
+                {/* // image 1   */}
+                <Carousel.Item>
+                  <img
+                    className="imgCarousel"
+                    // className="d-block w-100 "
+                    src={imgPhoto.localAddress}
+                    alt={imgPhoto.name}
+                  />
+                  <Carousel.Caption>
+                    <h3 id="carouselH3">{imgPhoto.message}</h3>
+                    <p> </p>
+                  </Carousel.Caption>
+                </Carousel.Item>
+              </Carousel>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button
+                onClick={() => {
+                  // setModal(false);
+                  setImageCarousel(false);
+                }}
+              >
+                Close
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        )}
+      </>
     );
   });
 };
