7a9c4fde2a446f4e33981a279df9511d450274ba
Shalom Dawit <Shalom.Dawit@gmail.com>
2022-07-11 02:14:50 -0400

    PhotoCollectionList: cleaned comments a little

--- client/src/components/PhotoCollectionList/index.js
+++ client/src/components/PhotoCollectionList/index.js
@@ -12,35 +12,13 @@ import "./style.css";
 
 const PhotoCollectionList = ({
   callbackAfterImagePress,
-  // startImageCarousel,
-  // imageCarouselInList,
-  // modalCarouselShowInList,
-  // modalHandleCloseInList,
 }) => {
   const [imgPhotos] = useContext(PhotoCollectionContext);
-  // const [index2, setIndex2] = useState(0);
-  // const [imageCarousel, setImageCarousel] = useState(false);
 
   const handleSelect = (selectedIndex, e) => {
     // setIndex2(selectedIndex);
   };
 
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-  // const handleClick = () => {
-  //   setImageCarousel(true);
-
-  //   console.log(`index onClick is ${index2}`);
-  // };
-  // console.log(`index onClick is ${index2}`);
-  // const [imageCarousel,setImageCarousel]=useState(false);
-  // carouselImageIdInList = index2;
-  // imageCarouselInList = imageCarousel;
-  // imageCarouselInList = 0;
-  // modalCarouselShowInList = show;
-  // modalHandleCloseInList = handleClose;
   // console.log(`index onClick is ${callbackAfterImagePress}`);
   return imgPhotos.map((imgPhoto) => {
     console.log(`imgPhoto is = ${imgPhoto.id}`);
@@ -66,15 +44,8 @@ const PhotoCollectionList = ({
           alt={imgPhoto.name}
           src={imgPhoto.localAddress}
           onClick={() => {
-            //   setModal(true);
-            // startImageCarousel(true);
-            // handleShow();
-            // setIndex2(imgPhoto.id);
             callbackAfterImagePress(imgPhoto);
-            // console.log(
-            //   `imageCarousel from PhotoCollection is ${imageCarousel}`
-            // );
-            // console.log("image has been clicked, next check prop variables");
+      
           }}
         />
       </div>
